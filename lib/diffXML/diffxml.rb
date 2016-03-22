2233d25ed24ff690a7d1a9191c42614dc51293b8
pbubnar <jake@pjbapps.com>
2016-03-22 16:53:28 -0400

    fixed a the xpath finder final name, and added a method seperately for the collection of them

--- lib/diffXML/diffxml.rb
+++ lib/diffXML/diffxml.rb
@@ -5,21 +5,15 @@ module DiffXML
   @xpathArray = []
   def self.compareXML(doc1)
     if doc1.class == Nokogiri::XML::Document
-      compareXML(doc1.root)
+      collectXPaths(doc1.root)
     else
-      doc1.element_children.each do |child|
-        if child.element_children.empty?
-          @xpathArray.push(self.getPath(child))
-        else
-          compareXML(child)
-        end
-      end
+      collectXPaths(doc1)
     end
-
+    @xpathArray.each {|element| compareToPath(element)}
   end
 
   def self.getPath(node, path = nil)
-    if path.parent.name.eql? 'Document'
+    if path.parent.name.eql? 'document'
       path
     else
       getPath(node.parent, "#{node.parent}/#{path}")
@@ -28,5 +22,15 @@ module DiffXML
 
   def self.compareToPath(path)
     @doc2.search(path, @doc1.collect_namespaces)
+  end
+
+  def self.collectXPaths(doc)
+    doc.element_children.each do |child|
+      if child.element_children.empty?
+        @xpathArray.push(self.getPath(child))
+      else
+        compareXML(child)
+      end
+    end
   end
 end
